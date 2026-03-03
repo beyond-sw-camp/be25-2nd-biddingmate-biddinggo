41126aa0f70e446b0be1c6e81412caeab8268157
seonwoo <49448330+melly8954@users.noreply.github.com>
2026-03-03 12:01:44 +0900

    chore: initial project setup

---
+++ src/main/java/com/biddingmate/biddinggo/BiddingGoApplication.java
@@ -0,0 +1,13 @@
+package com.biddingmate.biddinggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiddingGoApplication.class, args);
+    }
+
+}
