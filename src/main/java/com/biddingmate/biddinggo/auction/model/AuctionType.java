a21e7fccb0608f30cc1b6ddb06f3a93888fe0668
penep0 <>
2026-03-12 19:41:15 +0900

    feat: auction 및 item model 생성

---
+++ src/main/java/com/biddingmate/biddinggo/auction/model/AuctionType.java
@@ -0,0 +1,7 @@
+package com.biddingmate.biddinggo.auction.model;
+
+public enum AuctionType {
+    INSPECTION,
+    NORMAL,
+    TIME_DEAL
+}
---
+++ src/main/java/com/biddingmate/biddinggo/item/model/InspectionStatus.java
@@ -0,0 +1,8 @@
+package com.biddingmate.biddinggo.item.model;
+
+public enum InspectionStatus {
+    NONE,
+    PENDING,
+    PASSED,
+    FAILED
+}
