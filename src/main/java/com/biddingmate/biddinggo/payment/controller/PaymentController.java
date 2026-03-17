4ca85013ff5ecd71e681c109de92e9387534a57e
seonwoo <tjsdn8963@naver.com>
2026-03-17 15:00:29 +0900

    Merge pull request #27 from beyond-sw-camp/feature/virtual-account-deposit-webhook
    feat: 가상계좌 입금 웹 훅 API 구현

--- src/main/java/com/biddingmate/biddinggo/payment/controller/PaymentController.java
+++ src/main/java/com/biddingmate/biddinggo/payment/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.biddingmate.biddinggo.common.response.ApiResponse;
 import com.biddingmate.biddinggo.payment.dto.CreateVirtualAccountRequest;
 import com.biddingmate.biddinggo.payment.dto.CreateVirtualAccountResponse;
 import com.biddingmate.biddinggo.payment.dto.GetVirtualAccountResponse;
+import com.biddingmate.biddinggo.payment.dto.TossDepositWebhook;
 import com.biddingmate.biddinggo.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -33,7 +34,7 @@ public class PaymentController {
         return ApiResponse.of(HttpStatus.OK, null, "가상계좌 발급 성공", result);
     }
 
-    @GetMapping("virtual-accounts")
+    @GetMapping("/virtual-accounts")
 // 인증 구현 전이므로 인증 Principal을 담을 수 없기 때문에 @RequestParam으로 memberId 대체
 //    public ResponseEntity<ApiResponse<List<GetVirtualAccountResponse>>> getVirtualAccount(@AuthenticationPrincipal PrincipalDetails principal) {
 //        Member member = principal.getMember();
@@ -41,8 +42,18 @@ public class PaymentController {
     public ResponseEntity<ApiResponse<List<GetVirtualAccountResponse>>> getVirtualAccount(@RequestParam Long memberId) {
         List<GetVirtualAccountResponse> result = paymentService.getVirtualAccount(memberId);
 
-
-
         return ApiResponse.of(HttpStatus.OK, null, "가상계좌 조회 성공", result);
+    }
+
+    @PostMapping("/virtual-accounts/deposit")
+    public ResponseEntity<ApiResponse<Void>> handleTossDepositWebhook(@RequestBody TossDepositWebhook request) {
+        // 1. 로그로 데이터 확인 (가장 중요!)
+        System.out.println("토스 웹훅 도착!: \n" + request);
+
+        // 2. 서비스 로직 호출 (입금 완료 처리 등)
+        paymentService.processDeposit(request);
+
+        // 3. 토스에게 성공적으로 받았음을 알림 (200 OK)
+        return ApiResponse.of(HttpStatus.OK, null, "입금 및 포인트 충전 완료", null);
     }
 }
