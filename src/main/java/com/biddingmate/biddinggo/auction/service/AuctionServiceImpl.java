9345b7ae7cfd0ac77af07ddc403a7ebd88d03095
penep0 <>
2026-03-18 12:18:21 +0900

    feat: 검수 물품 경매 등록 로직 작성

--- src/main/java/com/biddingmate/biddinggo/auction/service/AuctionServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/auction/service/AuctionServiceImpl.java
@@ -1,8 +1,10 @@
 package com.biddingmate.biddinggo.auction.service;
 
+import com.biddingmate.biddinggo.auction.dto.CreateAuctionFromInspectionItemRequest;
 import com.biddingmate.biddinggo.auction.dto.CreateAuctionRequest;
 import com.biddingmate.biddinggo.auction.mapper.AuctionMapper;
 import com.biddingmate.biddinggo.auction.model.Auction;
+import com.biddingmate.biddinggo.auction.model.YesNo;
 import com.biddingmate.biddinggo.common.exception.CustomException;
 import com.biddingmate.biddinggo.common.exception.ErrorType;
 import lombok.RequiredArgsConstructor;
@@ -41,6 +43,35 @@ public class AuctionServiceImpl implements AuctionService {
                 .build();
 
         // auction 저장 후 생성된 PK를 모델에 주입받는다.
+        int auctionInsertCount = auctionMapper.insert(auction);
+
+        if (auctionInsertCount != 1 || auction.getId() == null) {
+            throw new CustomException(ErrorType.AUCTION_SAVE_FAILED);
+        }
+
+        return auction.getId();
+    }
+
+    @Override
+    public Long createAuction(CreateAuctionFromInspectionItemRequest request) {
+        if (request.getItemId() == null) {
+            throw new CustomException(ErrorType.INVALID_AUCTION_CREATE_REQUEST);
+        }
+
+        Auction auction = Auction.builder()
+                .itemId(request.getItemId())
+                .sellerId(request.getSellerId())
+                .type(request.getAuction().getType())
+                .inspectionYn(YesNo.YES)
+                .startPrice(request.getAuction().getStartPrice())
+                .bidUnit(request.getAuction().getBidUnit())
+                .vickreyPrice(request.getAuction().getVickreyPrice())
+                .buyNowPrice(request.getAuction().getBuyNowPrice())
+                .startDate(request.getAuction().getStartDate())
+                .endDate(request.getAuction().getEndDate())
+                .createdAt(LocalDateTime.now())
+                .build();
+
         int auctionInsertCount = auctionMapper.insert(auction);
 
         if (auctionInsertCount != 1 || auction.getId() == null) {
