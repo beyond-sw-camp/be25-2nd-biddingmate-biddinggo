d0c89783273c8bc908888ab754a9a44521b2102d
penep0 <>
2026-03-18 17:03:44 +0900

    feat: 검수 조회 및 업데이트 쿼리 작성

--- src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
+++ src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
@@ -2,8 +2,16 @@ package com.biddingmate.biddinggo.inspection.mapper;
 
 import com.biddingmate.biddinggo.common.inif.IMybatisCRUD;
 import com.biddingmate.biddinggo.inspection.model.Inspection;
+import com.biddingmate.biddinggo.inspection.model.InspectionStatus;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface InspectionMapper extends IMybatisCRUD<Inspection> {
+    int updateShippingInfo(
+            @Param("inspectionId") Long inspectionId,
+            @Param("carrier") String carrier,
+            @Param("trackingNumber") String trackingNumber,
+            @Param("currentStatus") InspectionStatus currentStatus
+    );
 }
