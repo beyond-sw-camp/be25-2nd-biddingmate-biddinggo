feaf1d477ba5b7eab266f6a482b0df0b3ceb0b50
penep0 <>
2026-03-18 19:43:34 +0900

    feat: 검수 상세 조회 쿼리 작성

--- src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
+++ src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
@@ -1,9 +1,11 @@
 package com.biddingmate.biddinggo.inspection.mapper;
 
 import com.biddingmate.biddinggo.common.inif.IMybatisCRUD;
+import com.biddingmate.biddinggo.inspection.dto.InspectionDetailResponse;
 import com.biddingmate.biddinggo.inspection.model.Inspection;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface InspectionMapper extends IMybatisCRUD<Inspection> {
+    InspectionDetailResponse findDetailById(Long inspectionId);
 }
