4c065b3421a7a1d369052e01fac693689f347815
softworldqjin <softworldqjin@gmail.com>
2026-03-22 18:49:14 +0900

    chore: Swagger 테스트를 위한 회원 프로필 조회 API 임시 접근 허용

--- src/main/java/com/biddingmate/biddinggo/config/SecurityConfig.java
+++ src/main/java/com/biddingmate/biddinggo/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                                 "/api/v1/admin-inquiries/**",
                                 "/api/v1/bidding/**",
                                 "/swagger-ui/**", "/v3/api-docs/**",
-                                "/api/v1/users/my"
+                                "/api/v1/users/my", "/api/v1/users/my/profile"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 );
