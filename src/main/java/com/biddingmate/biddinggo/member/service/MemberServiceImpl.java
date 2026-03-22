46a9e47911b8f76fc5cfb7715a2b132be40e93a7
softworldqjin <softworldqjin@gmail.com>
2026-03-22 22:50:12 +0900

    refactor: getMyInfo에서 memberExists 메서드로 회원 존재 여부 확인하도록 변경

--- src/main/java/com/biddingmate/biddinggo/member/service/MemberServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/member/service/MemberServiceImpl.java
@@ -22,12 +22,7 @@ public class MemberServiceImpl implements MemberService {
     public MemberMyResponse getMyInfo(Long memberId) {
 
         // 회원 존재 여부 체크
-        Member member = memberMapper.findById(memberId);
-
-        // 회원 존재하지 않을 시, 예외처리
-        if (member == null) {
-            throw new CustomException(ErrorType.MEMBER_NOT_FOUND);
-        }
+        memberExists(memberId);
 
         // 회원 존재 시, 조회
         return memberMapper.selectMyInfo(memberId);
