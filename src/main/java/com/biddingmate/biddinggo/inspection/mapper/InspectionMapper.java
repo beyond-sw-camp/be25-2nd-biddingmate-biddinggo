d519bb2a90d52d4b8c0f0552091f58c204c9aaf4
penep0 <>
2026-03-22 18:33:46 +0900

    feat: 검증 mapper 작성

--- src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
+++ src/main/java/com/biddingmate/biddinggo/inspection/mapper/InspectionMapper.java
@@ -2,10 +2,14 @@ package com.biddingmate.biddinggo.inspection.mapper;
 
 import com.biddingmate.biddinggo.common.inif.IMybatisCRUD;
 import com.biddingmate.biddinggo.inspection.dto.InspectionDetailResponse;
+import com.biddingmate.biddinggo.inspection.dto.InspectionListResponse;
 import com.biddingmate.biddinggo.inspection.model.Inspection;
 import com.biddingmate.biddinggo.inspection.model.InspectionStatus;
+import com.biddingmate.biddinggo.item.model.ItemInspectionStatus;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface InspectionMapper extends IMybatisCRUD<Inspection> {
@@ -15,5 +19,9 @@ public interface InspectionMapper extends IMybatisCRUD<Inspection> {
             @Param("trackingNumber") String trackingNumber,
             @Param("currentStatus") InspectionStatus currentStatus
     );
+
+    List<InspectionListResponse> findInspectionList(@Param("memberId") Long memberId,
+                                                    @Param("status") ItemInspectionStatus status);
+
     InspectionDetailResponse findDetailById(Long inspectionId);
 }
