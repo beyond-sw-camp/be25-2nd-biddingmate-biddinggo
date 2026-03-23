13d79d73003ccca8301482220401aba8a7b8ec16
softworldqjin <softworldqjin@gmail.com>
2026-03-23 22:56:24 +0900

    refactor: MemberService/MemberServiceImpl 메서드 네이밍 변경 및 로직 정리

--- src/main/java/com/biddingmate/biddinggo/member/service/MemberService.java
+++ src/main/java/com/biddingmate/biddinggo/member/service/MemberService.java
@@ -1,10 +1,10 @@
 package com.biddingmate.biddinggo.member.service;
 
-import com.biddingmate.biddinggo.member.dto.MemberMyResponse;
+import com.biddingmate.biddinggo.member.dto.MemberDashboardResponse;
 import com.biddingmate.biddinggo.member.dto.MemberProfileResponse;
 
 public interface MemberService {
-    MemberMyResponse getMyInfo(Long memberId);
+    MemberDashboardResponse getMyDashboard(Long memberId);
 
     MemberProfileResponse getMyProfile(Long memberId);
 }
--- src/main/java/com/biddingmate/biddinggo/member/service/MemberServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/member/service/MemberServiceImpl.java
@@ -2,8 +2,7 @@ package com.biddingmate.biddinggo.member.service;
 
 import com.biddingmate.biddinggo.common.exception.CustomException;
 import com.biddingmate.biddinggo.common.exception.ErrorType;
-import com.biddingmate.biddinggo.member.dto.MemberMyResponse;
-import com.biddingmate.biddinggo.member.dto.MemberProfileQueryResponse;
+import com.biddingmate.biddinggo.member.dto.MemberDashboardResponse;
 import com.biddingmate.biddinggo.member.dto.MemberProfileResponse;
 import com.biddingmate.biddinggo.member.mapper.MemberMapper;
 import com.biddingmate.biddinggo.member.model.Member;
@@ -19,13 +18,13 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberMapper memberMapper;
     @Override
-    public MemberMyResponse getMyInfo(Long memberId) {
+    public MemberDashboardResponse getMyDashboard(Long memberId) {
 
         // 회원 존재 여부 체크
         memberExists(memberId);
 
-        // 회원 존재 시, 조회
-        return memberMapper.selectMyInfo(memberId);
+        // 대시보드 정보 조회
+        return memberMapper.findDashboardById(memberId);
     }
 
     @Override
@@ -34,44 +33,12 @@ public class MemberServiceImpl implements MemberService {
         // 회원 존재 여부 체크
         memberExists(memberId);
 
-        MemberProfileQueryResponse queryResult = memberMapper.selectMyProfile(memberId);
-
-        boolean canChangeNickname = false;
-        int remainingDays = 0;
-
-        // 닉네임을 한번도 변경하지 않았을 경우
-        if (queryResult.getLastChangeNick() == null) {
-            canChangeNickname = true;
-
-            // 닉네임을 변경한 적이 있을 경우
-        } else {
-            LocalDate today = LocalDate.now();
-            LocalDate changeableDate = queryResult.getLastChangeNick().toLocalDate().plusDays(30);
-
-            // 30일이 지나야 닉네임 변경가능, 30일 되는 날은 불가능
-            if (today.isAfter(changeableDate)) {
-                canChangeNickname = true;
-            } else {
-                remainingDays = (int) ChronoUnit.DAYS.between(today, changeableDate);
-            }
-        }
-
-        return MemberProfileResponse.builder()
-                .imageUrl(queryResult.getImageUrl())
-                .name(queryResult.getName())
-                .nickname(queryResult.getNickname())
-                .email(queryResult.getEmail())
-                .zipcode(queryResult.getZipcode())
-                .address(queryResult.getAddress())
-                .detailAddress(queryResult.getDetailAddress())
-                .bankCode(queryResult.getBankCode())
-                .bankAccount(queryResult.getBankAccount())
-                .canChangeNickname(canChangeNickname)
-                .remainingDays(remainingDays)
-                .build();
+        // 프로필 정보 조회
+        return memberMapper.findProfileById(memberId);
     }
 
     private void memberExists(Long memberId) {
+        // 회원 단건 조회
         Member member = memberMapper.findById(memberId);
 
         // 회원 존재하지 않을 시, 예외처리
