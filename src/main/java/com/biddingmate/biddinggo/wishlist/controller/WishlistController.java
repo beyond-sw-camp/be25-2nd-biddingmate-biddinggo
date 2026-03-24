7427a06a4c3a4bb03075f2945a73f3f56bafb49c
nueeaeel <dodms1006@naver.com>
2026-03-25 02:30:12 +0900

    feat: 관심 경매 조회 구현

--- src/main/java/com/biddingmate/biddinggo/wishlist/controller/WishlistController.java
+++ src/main/java/com/biddingmate/biddinggo/wishlist/controller/WishlistController.java
@@ -1,15 +1,21 @@
 package com.biddingmate.biddinggo.wishlist.controller;
 
+import com.biddingmate.biddinggo.auction.dto.AuctionDetailResponse;
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
 import com.biddingmate.biddinggo.common.response.ApiResponse;
+import com.biddingmate.biddinggo.common.response.PageResponse;
+import com.biddingmate.biddinggo.wishlist.dto.CreateWishlistRequest;
 import com.biddingmate.biddinggo.wishlist.dto.CreateWishlistResponse;
+import com.biddingmate.biddinggo.wishlist.dto.WishlistCountResponse;
 import com.biddingmate.biddinggo.wishlist.service.WishlistService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +28,10 @@ import software.amazon.awssdk.annotations.NotNull;
 public class WishlistController {
     private final WishlistService wishlistService;
 
-    @PostMapping("/{auctionId}")
+    @PostMapping("/")
     @Operation(summary = "관심 경매 등록", description = "관심 경매를 등록합니다.")
     public ResponseEntity<ApiResponse<CreateWishlistResponse>> createWishlist(
-            @NotNull @PathVariable Long auctionId,
+            @RequestBody CreateWishlistRequest request,
             /*
               <<to-do>>
               이후 인증 구현 완료 후, 로그인 정보 받아와서 memberId에 주입
@@ -33,8 +39,33 @@ public class WishlistController {
             @NotNull @RequestParam Long memberId
     ) {
 
-        CreateWishlistResponse result = wishlistService.createWishlist(memberId, auctionId);
+        CreateWishlistResponse result = wishlistService.createWishlist(request, memberId);
 
         return ApiResponse.of(HttpStatus.OK, null, "관심 경매 등록 성공", result);
+    }
+
+    @GetMapping("/count")
+    @Operation(summary = "경매 관심 수 조회", description = "경매의 관심 수를 조회합니다.")
+    public ResponseEntity<ApiResponse<WishlistCountResponse>> getWishlistCount(
+            @NotNull @RequestParam Long auctionId
+    ){
+        WishlistCountResponse result = wishlistService.getWishlistCount(auctionId);
+
+        return ApiResponse.of(HttpStatus.OK, null, "경매 관심 수 조회 성공", result);
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "내 관심 경매 조회", description = "사용자의 관심 경매를 조회합니다.")
+    public ResponseEntity<ApiResponse<PageResponse<AuctionDetailResponse>>> getWishlist(
+            BasePageRequest request,
+            /*
+              <<to-do>>
+              이후 인증 구현 완료 후, 로그인 정보 받아와서 memberId에 주입
+             */
+            @NotNull @RequestParam Long memberId
+    ) {
+        PageResponse<AuctionDetailResponse> result = wishlistService.findWishlistAuctionsByMemberId(request, memberId);
+
+        return ApiResponse.of(HttpStatus.OK, null, "관심 경매 조회 성공", result);
     }
 }
--- src/main/java/com/biddingmate/biddinggo/wishlist/service/WishlistServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/wishlist/service/WishlistServiceImpl.java
@@ -1,26 +1,36 @@
 package com.biddingmate.biddinggo.wishlist.service;
 
+import com.biddingmate.biddinggo.auction.dto.AuctionDetailResponse;
 import com.biddingmate.biddinggo.auction.mapper.AuctionMapper;
+import com.biddingmate.biddinggo.auction.service.AuctionService;
 import com.biddingmate.biddinggo.common.exception.CustomException;
 import com.biddingmate.biddinggo.common.exception.ErrorType;
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
+import com.biddingmate.biddinggo.common.response.PageResponse;
+import com.biddingmate.biddinggo.wishlist.dto.CreateWishlistRequest;
 import com.biddingmate.biddinggo.wishlist.dto.CreateWishlistResponse;
+import com.biddingmate.biddinggo.wishlist.dto.WishlistCountResponse;
 import com.biddingmate.biddinggo.wishlist.mapper.WishlistMapper;
 import com.biddingmate.biddinggo.wishlist.model.Wishlist;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WishlistServiceImpl implements WishlistService {
     private final AuctionMapper auctionMapper;
     private final WishlistMapper wishlistMapper;
+    private final AuctionService auctionService;
 
     @Override
     @Transactional
-    public CreateWishlistResponse createWishlist(Long memberId, Long auctionId) {
+    public CreateWishlistResponse createWishlist(CreateWishlistRequest request, Long memberId) {
+        Long auctionId = request.getAuctionId();
         if(auctionMapper.findById(auctionId) == null){
             throw new CustomException(ErrorType.AUCTION_NOT_FOUND);
         }
@@ -47,5 +57,35 @@ public class WishlistServiceImpl implements WishlistService {
                 .auctionId(wishlist.getAuctionId())
                 .createdAt(wishlist.getCreatedAt())
                 .build();
+    }
+
+    @Override
+    public WishlistCountResponse getWishlistCount(Long auctionId) {
+        if(auctionMapper.findById(auctionId) == null){
+            throw new CustomException(ErrorType.AUCTION_NOT_FOUND);
+        }
+
+        int count = wishlistMapper.getCountByAuctionId(auctionId);
+
+        return WishlistCountResponse.builder()
+                .auctionId(auctionId)
+                .count(count)
+                .build();
+    }
+
+    @Override
+    public PageResponse<AuctionDetailResponse> findWishlistAuctionsByMemberId(BasePageRequest request, Long memberId) {
+        RowBounds rowBounds = new RowBounds(request.getOffset(), request.getSize());
+        String order = request.getOrder();
+
+        if (!"ASC".equalsIgnoreCase(order) && !"DESC".equalsIgnoreCase(order)) {
+            throw new CustomException(ErrorType.INVALID_SORT_ORDER);
+        }
+        String sortOrder = order.toUpperCase();
+
+        List<AuctionDetailResponse> auctions = wishlistMapper.findWishlistAuctionsByMemberId(rowBounds, memberId, sortOrder);
+        int count = wishlistMapper.getCountByMemberId(memberId);
+
+        return PageResponse.of(auctions, request.getPage(), request.getSize(), count);
     }
 }
