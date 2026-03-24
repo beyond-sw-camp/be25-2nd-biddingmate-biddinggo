466e76586304752e756644f13f99a4d1a8834f23
penep0 <>
2026-03-24 10:22:37 +0900

    refactor: InspectionListRequest DTO page 상속

--- src/main/java/com/biddingmate/biddinggo/inspection/dto/InspectionListRequest.java
+++ src/main/java/com/biddingmate/biddinggo/inspection/dto/InspectionListRequest.java
@@ -1,21 +1,22 @@
 package com.biddingmate.biddinggo.inspection.dto;
 
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Schema(description = "검수물품 목록 조회 요청 DTO")
-public class InspectionListRequest {
+public class InspectionListRequest extends BasePageRequest {
     @Schema(description = "회원 ID", example = "1")
     @NotNull(message = "회원 ID는 필수입니다.")
     @Positive(message = "회원 ID는 1 이상이어야 합니다.")
