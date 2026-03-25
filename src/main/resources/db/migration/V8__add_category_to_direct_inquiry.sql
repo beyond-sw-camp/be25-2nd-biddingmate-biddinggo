e3b1eb466a0f7f2dec373390465ad4d7ff6f61d1
haily <49448330+melly8954@users.noreply.github.com>
2026-03-26 00:07:58 +0900

    fix: flyway migration v__8 수정

--- src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
+++ src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
@@ -1,3 +1,2 @@
 ALTER TABLE direct_inquiry
-    MODIFY COLUMN `admin_id` BIGINT(20) NULL,
-    ADD COLUMN category VARCHAR(20) NOT NULL AFTER admin_id;
+    MODIFY COLUMN category VARCHAR(20) NOT NULL;
