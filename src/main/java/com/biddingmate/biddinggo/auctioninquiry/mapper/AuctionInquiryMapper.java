f2dc0af81d37d1041b447eb06ed7c9a5c4c62385
alskung1101 <alskung1101@naver.com>
2026-03-26 03:25:18 +0900

    feat: 경매 문의 목록 조회를 위한 DTO 정의 및 Mapper 인터페이스 추가

--- src/main/java/com/biddingmate/biddinggo/auctioninquiry/mapper/AuctionInquiryMapper.java
+++ src/main/java/com/biddingmate/biddinggo/auctioninquiry/mapper/AuctionInquiryMapper.java
@@ -1,8 +1,13 @@
 package com.biddingmate.biddinggo.auctioninquiry.mapper;
 
+import com.biddingmate.biddinggo.auctioninquiry.dto.AuctionInquiryView;
 import com.biddingmate.biddinggo.auctioninquiry.model.AuctionInquiry;
 import com.biddingmate.biddinggo.common.inif.IMybatisCRUD;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -11,4 +16,8 @@ public interface AuctionInquiryMapper extends IMybatisCRUD<AuctionInquiry> {
     Optional<AuctionInquiry> findInquiryById(Long id);
 
     int updateAnswer(AuctionInquiry inquiry);
+
+    List<AuctionInquiryView> selectInquiryList(RowBounds rowBounds, @Param("auctionId") Long auctionId);
+
+    int selectInquiryCount(@Param("auctionId") Long auctionId);
 }
