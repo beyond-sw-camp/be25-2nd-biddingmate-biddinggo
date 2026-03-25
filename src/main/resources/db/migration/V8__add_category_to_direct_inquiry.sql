36b607b8612be098b8b4706458fca60e6965e734
seonwoo <49448330+melly8954@users.noreply.github.com>
2026-03-25 14:36:01 +0900

    feat: flyway-migration-v8

--- src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
+++ src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
@@ -1,3 +1,3 @@
 ALTER TABLE direct_inquiry
-    ADD COLUMN category VARCHAR(20) DEFAULT NULL AFTER admin_id,
+    ADD COLUMN category VARCHAR(20) NOT NULL AFTER admin_id,
     MODIFY COLUMN `admin_id` BIGINT(20) NULL;
