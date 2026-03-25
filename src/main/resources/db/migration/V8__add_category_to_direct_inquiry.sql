d85506aff23589907bd11fb02ecb122138bf2383
seonwoo <49448330+melly8954@users.noreply.github.com>
2026-03-25 14:53:57 +0900

    refactor: flyway-migration-v8
    DML 변경

--- src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
+++ src/main/resources/db/migration/V8__add_category_to_direct_inquiry.sql
@@ -1,3 +1,3 @@
 ALTER TABLE direct_inquiry
-    ADD COLUMN category VARCHAR(20) NOT NULL AFTER admin_id,
-    MODIFY COLUMN `admin_id` BIGINT(20) NULL;
+    MODIFY COLUMN `admin_id` BIGINT(20) NULL,
+    ADD COLUMN category VARCHAR(20) NOT NULL AFTER admin_id;
