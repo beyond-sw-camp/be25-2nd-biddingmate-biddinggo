49cd9674616a49fd1e24a20e4325745065a8903d
softworldqjin <softworldqjin@gmail.com>
2026-03-25 11:53:23 +0900

    feat: MemberDashboardResponse 구조 변경 (회원 정보 및 낙찰/ 입찰 목록 포함)

--- src/main/java/com/biddingmate/biddinggo/member/dto/MemberDashboardResponse.java
+++ src/main/java/com/biddingmate/biddinggo/member/dto/MemberDashboardResponse.java
@@ -5,14 +5,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class MemberDashboardResponse {
-    private Long totalPurchaseAmount;
 
-    private Long totalSalesAmount;
-
+    // 대시보드에 노출될 회원 정보
+    private String nickname;
+    private String grade;
     private Long point;
+
+    // 낙찰된 물품 목록
+    private List<MemberWonItemResponse> wonItems;
+
+    // 입찰 중 물품 목록
+    private List<MemberBiddingItemResponse> biddingItems;
+
 }
