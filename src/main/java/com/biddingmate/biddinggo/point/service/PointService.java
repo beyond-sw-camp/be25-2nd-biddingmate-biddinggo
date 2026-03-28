6bf1449dbe167da6d85e3b263f27b23706a6274f
haily <49448330+melly8954@users.noreply.github.com>
2026-03-28 18:39:26 +0900

    feat: 사용자 포인트 히스토리 내역 조회 비즈니스 로직 및 Mapper

--- src/main/java/com/biddingmate/biddinggo/point/service/PointService.java
+++ src/main/java/com/biddingmate/biddinggo/point/service/PointService.java
@@ -1,5 +1,8 @@
 package com.biddingmate.biddinggo.point.service;
 
-public interface PointService {
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
+import com.biddingmate.biddinggo.point.dto.MyPointResponse;
 
+public interface PointService {
+    MyPointResponse findMyPointList(BasePageRequest request, Long memberId);
 }
--- src/main/java/com/biddingmate/biddinggo/point/service/PointServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/point/service/PointServiceImpl.java
@@ -1,5 +1,51 @@
 package com.biddingmate.biddinggo.point.service;
 
-public class PointServiceImpl implements PointService {
+import com.biddingmate.biddinggo.common.exception.CustomException;
+import com.biddingmate.biddinggo.common.exception.ErrorType;
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
+import com.biddingmate.biddinggo.common.response.PageResponse;
+import com.biddingmate.biddinggo.directinquiry.dto.DirectInquiryView;
+import com.biddingmate.biddinggo.member.service.MemberService;
+import com.biddingmate.biddinggo.point.dto.MyPointResponse;
+import com.biddingmate.biddinggo.point.dto.PointHistoryDto;
+import com.biddingmate.biddinggo.point.mapper.PointHistoryMapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PointServiceImpl implements PointService {
+    private final MemberService memberService;
+    private final PointHistoryMapper pointHistoryMapper;
+
+    @Override
+    @Transactional(readOnly = true)
+    public MyPointResponse findMyPointList(BasePageRequest request, Long memberId) {
+        long currentPoint = memberService.getCurrentPoint(memberId);
+
+        RowBounds rowBounds = new RowBounds(request.getOffset(), request.getSize());
+        String order = request.getOrder();
+
+        if (!"ASC".equalsIgnoreCase(order) && !"DESC".equalsIgnoreCase(order)) {
+            throw new CustomException(ErrorType.INVALID_SORT_ORDER);
+        }
+        String sortOrder = order.toUpperCase();
+
+        List<PointHistoryDto> list;
+        int count;
+
+        list = pointHistoryMapper.findByMemberId(rowBounds, sortOrder, memberId);
+        count = pointHistoryMapper.countByMemberId(memberId);
+
+        PageResponse<PointHistoryDto> pointhistory = PageResponse.of(list, request.getPage(), request.getSize(), count);
+
+        return MyPointResponse.builder()
+                .currentPoint(currentPoint)
+                .histroies(pointhistory)
+                .build();
+    }
 }
