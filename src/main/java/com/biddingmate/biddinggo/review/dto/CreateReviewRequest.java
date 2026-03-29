bb11c73c9ef12b13abc13c9dab363a37c412b1ab
alskung1101 <alskung1101@naver.com>
2026-03-29 19:20:22 +0900

    feat: dto 수정

--- src/main/java/com/biddingmate/biddinggo/review/dto/CreateReviewRequest.java
+++ src/main/java/com/biddingmate/biddinggo/review/dto/CreateReviewRequest.java
@@ -1,11 +1,17 @@
 package com.biddingmate.biddinggo.review.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateReviewRequest {
     @NotNull(message = "별점은 필수입니다.")
     @Min(1) @Max(5) // 별점 1~5점 제한
