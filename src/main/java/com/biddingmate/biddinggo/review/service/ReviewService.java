6fd53b11a39b2da22e36ca3af969f69fed999297
alskung1101 <alskung1101@naver.com>
2026-03-29 19:21:51 +0900

    feat: 주석 제거

--- src/main/java/com/biddingmate/biddinggo/review/service/ReviewService.java
+++ src/main/java/com/biddingmate/biddinggo/review/service/ReviewService.java
@@ -2,7 +2,6 @@ package com.biddingmate.biddinggo.review.service;
 
 import com.biddingmate.biddinggo.review.dto.CreateReviewRequest;
 import com.biddingmate.biddinggo.review.dto.CreateReviewResponse;
-//import com.biddingmate.biddinggo.review.dto.ReviewView;
 
 public interface ReviewService {
     // 리뷰 등록
