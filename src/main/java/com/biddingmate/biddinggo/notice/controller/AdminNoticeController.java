b78f68218f2a0c8404564e6e3b45d5fb345019e1
jin605 <jinddd3@gmail.com>
2026-04-02 20:28:30 +0900

    feat : 유저 조회/ 관리자 조회

--- src/main/java/com/biddingmate/biddinggo/notice/controller/AdminNoticeController.java
+++ src/main/java/com/biddingmate/biddinggo/notice/controller/AdminNoticeController.java
@@ -1,10 +1,13 @@
 package com.biddingmate.biddinggo.notice.controller;
 
+import com.biddingmate.biddinggo.common.request.BasePageRequest;
 import com.biddingmate.biddinggo.common.response.ApiResponse;
+import com.biddingmate.biddinggo.common.response.PageResponse;
 import com.biddingmate.biddinggo.notice.dto.CreateNoticeRequest;
 import com.biddingmate.biddinggo.notice.dto.NoticeResponse;
 import com.biddingmate.biddinggo.notice.dto.UpdateNoticeRequest;
 import com.biddingmate.biddinggo.notice.service.AdminNoticeService;
+import com.biddingmate.biddinggo.notice.service.NoticeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -13,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +32,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminNoticeController {
 
     private final AdminNoticeService adminNoticeService;
+    private final NoticeService noticeService;
+
+    @GetMapping("")
+    @Operation(summary = "관리자 공지사항 목록 조회")
+    public ResponseEntity<ApiResponse<PageResponse<NoticeResponse>>> findAllNotices (BasePageRequest request) {
+        PageResponse<NoticeResponse> result = noticeService.findAllNotices(request);
+        return ApiResponse.of(HttpStatus.OK, null, "관리자 공지사항 목록 조회 성공", result);
+    }
 
 
     @PostMapping
@@ -62,5 +74,7 @@ public class AdminNoticeController {
 
         return ApiResponse.<Void>of(HttpStatus.OK, null, "공지 사항 삭제 완료", null);
     }
+
+
 
 }
