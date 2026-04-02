697aa197c96e85b2c628cd5154381bb7772f5a77
jin605 <jinddd3@gmail.com>
2026-04-02 17:18:53 +0900

    feat : 에러타입 추가

---
+++ src/main/java/com/biddingmate/biddinggo/notice/mapper/NoticeMapper.java
@@ -0,0 +1,8 @@
+package com.biddingmate.biddinggo.notice.mapper;
+
+import com.biddingmate.biddinggo.common.inif.IMybatisCRUD;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NoticeMapper extends IMybatisCRUD {
+}
--- src/main/java/com/biddingmate/biddinggo/notice/service/AdminNoticeServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/notice/service/AdminNoticeServiceImpl.java
@@ -2,10 +2,14 @@ package com.biddingmate.biddinggo.notice.service;
 
 import com.biddingmate.biddinggo.notice.dto.NoticeRequest;
 import com.biddingmate.biddinggo.notice.dto.NoticeResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminNoticeServiceImpl implements AdminNoticeService {
+
+
     @Override
     public NoticeResponse create(NoticeRequest request) {
         return null;
