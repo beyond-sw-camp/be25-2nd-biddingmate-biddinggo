01aa0fdd1a52686f6b7ec85cfbece7c89ee6f332
haily <49448330+melly8954@users.noreply.github.com>
2026-04-05 21:17:36 +0900

    feat: Address 도메인 API Swagger 명세서 등록

--- src/main/java/com/biddingmate/biddinggo/address/controller/AddressController.java
+++ src/main/java/com/biddingmate/biddinggo/address/controller/AddressController.java
@@ -1,4 +1,4 @@
-package com.biddingmate.biddinggo.address.controller;
+﻿package com.biddingmate.biddinggo.address.controller;
 
 import com.biddingmate.biddinggo.address.dto.AddressListResponse;
 import com.biddingmate.biddinggo.address.dto.CreateAddressRequest;
@@ -7,6 +7,7 @@ import com.biddingmate.biddinggo.address.service.AddressService;
 import com.biddingmate.biddinggo.common.response.ApiResponse;
 import com.biddingmate.biddinggo.member.model.Member;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -32,35 +33,39 @@ public class AddressController {
     private final AddressService addressService;
 
     @PostMapping("")
-    @Operation(summary = "배송지 등록", description = "새로운 배송지를 등록합니다.")
-    public ResponseEntity<ApiResponse<CreateAddressResponse>> createAddress(@Valid @RequestBody CreateAddressRequest request,
-                                                                            @AuthenticationPrincipal Member member) {
+    @Operation(summary = "배송지 등록", description = "새 배송지를 등록합니다.")
+    public ResponseEntity<ApiResponse<CreateAddressResponse>> createAddress(
+            @Valid @RequestBody CreateAddressRequest request,
+            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         CreateAddressResponse result = addressService.createAddress(request, member.getId());
 
         return ApiResponse.of(HttpStatus.OK, null, "배송지 등록 성공", result);
     }
 
     @GetMapping("")
-    @Operation(summary = "배송지 목록 조회", description = "사용자의 배송지 목록을 조회합니다.")
-    public ResponseEntity<ApiResponse<List<AddressListResponse>>> findAllAddress(@AuthenticationPrincipal Member member) {
+    @Operation(summary = "배송지 목록 조회", description = "배송지 목록을 조회합니다.")
+    public ResponseEntity<ApiResponse<List<AddressListResponse>>> findAllAddress(
+            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         List<AddressListResponse> result = addressService.findAllAddress(member.getId());
 
         return ApiResponse.of(HttpStatus.OK, null, "배송지 조회 성공", result);
     }
 
     @PatchMapping("/{addressId}")
-    @Operation(summary = "기본 배송지 변경", description = "선택한 배송지를 기본 배송지로 변경합니다.")
-    public ResponseEntity<ApiResponse<Void>> updateDefault(@PathVariable Long addressId,
-                                                           @AuthenticationPrincipal Member member) {
+    @Operation(summary = "기본 배송지 변경", description = "기본 배송지로 변경합니다.")
+    public ResponseEntity<ApiResponse<Void>> updateDefault(
+            @Parameter(description = "배송지 ID", example = "1") @PathVariable Long addressId,
+            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         addressService.updateDefaultAddress(addressId, member.getId());
 
         return ApiResponse.of(HttpStatus.OK, null, "기본 배송지 변경에 성공했습니다.", null);
     }
 
     @DeleteMapping("/{addressId}")
-    @Operation(summary = "배송지 삭제", description = "선택한 배송지를 삭제합니다.")
-    public ResponseEntity<ApiResponse<Void>> deleteAddress(@PathVariable Long addressId,
-                                                           @AuthenticationPrincipal Member member) {
+    @Operation(summary = "배송지 삭제", description = "배송지를 삭제합니다.")
+    public ResponseEntity<ApiResponse<Void>> deleteAddress(
+            @Parameter(description = "배송지 ID", example = "1") @PathVariable Long addressId,
+            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         addressService.deleteAddress(addressId, member.getId());
 
         return ApiResponse.of(HttpStatus.OK, null, "배송지 삭제를 성공했습니다.", null);
--- src/main/java/com/biddingmate/biddinggo/address/dto/CreateAddressResponse.java
+++ src/main/java/com/biddingmate/biddinggo/address/dto/CreateAddressResponse.java
@@ -1,5 +1,6 @@
 package com.biddingmate.biddinggo.address.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,6 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @Builder
+@Schema(description = "배송지 등록 응답 DTO")
 public class CreateAddressResponse {
+    @Schema(description = "배송지 ID", example = "1")
     private Long id;
 }
