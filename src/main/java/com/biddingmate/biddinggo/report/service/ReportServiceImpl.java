eab5f22293d4dfeaa1ab7e40fa3e4c03f46fe106
alskung1101 <alskung1101@naver.com>
2026-04-11 18:01:34 +0900

    fix: 유저 중복 비활성화 방지 조건 추가 및 불필요 매퍼 정리

--- src/main/java/com/biddingmate/biddinggo/report/service/ReportServiceImpl.java
+++ src/main/java/com/biddingmate/biddinggo/report/service/ReportServiceImpl.java
@@ -1,14 +1,11 @@
 package com.biddingmate.biddinggo.report.service;
 
-import com.biddingmate.biddinggo.auction.mapper.AuctionMapper;
-import com.biddingmate.biddinggo.bid.mapper.BidMapper;
 import com.biddingmate.biddinggo.common.exception.CustomException;
 import com.biddingmate.biddinggo.common.exception.ErrorType;
 import com.biddingmate.biddinggo.member.event.MemberStatusUpdateEvent;
 import com.biddingmate.biddinggo.member.mapper.MemberMapper;
 import com.biddingmate.biddinggo.member.model.Member;
 import com.biddingmate.biddinggo.member.model.MemberStatus;
-import com.biddingmate.biddinggo.point.service.PointService;
 import com.biddingmate.biddinggo.report.dto.ReportCreateRequest;
 import com.biddingmate.biddinggo.report.mapper.ReportMapper;
 import com.biddingmate.biddinggo.report.model.Report;
@@ -25,9 +22,6 @@ public class ReportServiceImpl implements ReportService {
 
     private final ReportMapper reportMapper;
     private final MemberMapper memberMapper;
-    private final AuctionMapper auctionMapper;
-    private final BidMapper bidMapper;
-    private final PointService pointService;
     private final ApplicationEventPublisher eventPublisher;
 
     @Override
@@ -38,10 +32,12 @@ public class ReportServiceImpl implements ReportService {
         if (request.getTargetMemberId().equals(reporterId)) {
             throw new CustomException(ErrorType.CANNOT_REPORT_SELF);
         }
-//        // 중복 신고 검증
-//        if (reportMapper.existsByReporterIdAndTargetId(reporterId, request.getTargetMemberId(), "MEMBER")) {
-//            throw new CustomException(ErrorType.ALREADY_REPORTED);
-//        }
+
+        // 중복 신고 검증
+        if (reportMapper.existsByReporterIdAndTargetId(reporterId, request.getTargetMemberId(), "MEMBER")) {
+            throw new CustomException(ErrorType.ALREADY_REPORTED);
+        }
+
         // 동시성 제어를 위한 유저 락
         Member targetMember = memberMapper.findByIdForUpdate(request.getTargetMemberId());
 
@@ -60,7 +56,7 @@ public class ReportServiceImpl implements ReportService {
         // 누적 횟수 확인 (3회 이상 시 커스텀 정지 로직 실행)
         int reportCount = reportMapper.countByTargetMemberId(request.getTargetMemberId());
 
-        if (reportCount >= 3 && targetMember.getStatus() != MemberStatus.ACTIVE) {
+        if (reportCount >= 3 && targetMember.getStatus() == MemberStatus.ACTIVE) {
             processMemberDeactivation(request.getTargetMemberId());
         }
     }
